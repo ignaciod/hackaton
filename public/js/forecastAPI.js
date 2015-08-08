e8ed4d55f201f2b78af011dc6a0ca2c34c38e498
matiasrad <matiasrad@pedidosya.com>
2015-08-08 14:28:35 -0300

    Agregada API climatica :boom: :shit: :pig:

---
+++ public/js/forecastAPI.js
@@ -0,0 +1,39 @@
+var forecast = (function() {
+
+	var TOKEN = '6aaa9efe005671c0657a63fe518ab6b1';
+
+    var getForecast = function(lat, lng, callback) {
+    	var reqUrl = 'https://api.forecast.io/forecast/' + TOKEN + '/' + lat + ',' + lng;
+    	var params = {units: 'si', exclude: 'minutely,hourly,alerts,flags'};
+
+    	$.ajax({
+    		url: reqUrl,
+    		type: 'GET',
+    		dataType: 'jsonp',
+    		data: params,
+    	})
+    	.done(function(data) {
+            if (callback instanceof Function) {
+                var obj = { temperature: data.currently.temperature, max: data.daily.data[0].temperatureMax,
+                            min: data.daily.data[0].temperatureMin, icon: data.currently.icon };
+
+                callback(obj);
+            }
+    	})
+    	.fail(function() {
+    		console.log("error retrieving the forecast for " + lat + ', ' + lng);
+    	});
+    }
+
+    return {
+        getForecast:getForecast
+    }
+})();
+
+$(document).ready(function(){
+    $('#btnHola').click(function(event) {
+        forecast.getForecast('-32.5583167', '-55.8117021', function(f) {
+            console.log(f);
+        });
+    });
+});
