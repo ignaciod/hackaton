ce57a98721a48d1980f7b69e30f2c28890781b54
Beto <agaleotti@gmail.com>
2015-08-08 16:14:08 -0300

    wip templating with solution

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,27 +1,21 @@
-$(function () {
+var template = (function(){
+  var theCompiledHtml = '';
+  var _getTemplate = function (data) {
 
-  // // Register a helper
-  // Handlebars.registerHelper('capitalize', function(str){
-  //   // str is the argument passed to the helper when called
-  //   str = str || '';
-  //   return str.slice(0,1).toUpperCase() + str.slice(1);
-  // });
+    var el = $('#tmp'+data.type);
+    
+    // Grab the template script
+    var theTemplateScript = $(el).html();
 
-  // Grab the template script
-  var theTemplateScript = $("#tmpTextImage").html();
-
-  // Compile the template
-  var theTemplate = Handlebars.compile(theTemplateScript);
-
-  // We will call this template on an array of objects
-  var context = {
-    text : 'llalalalalalallala'
+    // Compile the template
+    var theTemplate = Handlebars.compile(theTemplateScript);
+    // Pass our data to the template
+    theCompiledHtml = theTemplate(data);
+    
+    return theCompiledHtml;
   };
+  return {
+    get:_getTemplate()
+  }
+})();
 
-  // Pass our data to the template
-  var theCompiledHtml = theTemplate(context);
-
-  // Add the compiled html to the page
-  $('#big').html(theCompiledHtml);
-
-});
